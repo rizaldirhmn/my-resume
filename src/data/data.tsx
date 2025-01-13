406303412231aafe15b192dd3e4b2e97373a3686
rizaldihybrid <rizaldi@hybridbooking.com>
2025-01-13 10:16:31 +0800

    update data

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -11,7 +11,7 @@ import {
 import GithubIcon from '../components/Icon/GithubIcon';
 import InstagramIcon from '../components/Icon/InstagramIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
-import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
+// import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
 import TwitterIcon from '../components/Icon/TwitterIcon';
 import heroImage from '../images/header-background.webp';
 import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
@@ -44,8 +44,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: 'Rizaldi Rahman Resume',
+  description: "Welcome to My Resume",
 };
 
 /**
@@ -55,11 +55,11 @@ export const SectionId = {
   Hero: 'hero',
   About: 'about',
   Contact: 'contact',
-  Portfolio: 'portfolio',
+  // Portfolio: 'portfolio',
   Resume: 'resume',
   Skills: 'skills',
   Stats: 'stats',
-  Testimonials: 'testimonials',
+  // Testimonials: 'testimonials',
 } as const;
 
 export type SectionId = (typeof SectionId)[keyof typeof SectionId];
@@ -69,18 +69,11 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `I'm Rizaldi Rahman.`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
-      </p>
-      <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        I'm a Full Stack Software Engineer, currently working at <a href="https://www.hybridbooking.com/" className="font-bold">Hybrid Booking</a> helping update and build a modern Reservation System based on Bali, Indonesia.
       </p>
     </>
   ),
@@ -104,16 +97,15 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
-  to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
-  in.`,
+  description: `I'm an Extraverted Person, i can easily enjoy with people i've just meet. Now i'm enjoyed to explore an AI Technology, based on my job before i'm continue to explore how the AI Chatbot Working build with Python.
+  But for professional things as Software Engineer currently i'm working with React JS/TS for Frontend Development and using Express JS/TS for Backend Development, i used to work with MySQL for DBMS. Besides that most of the time i'm working with PHP (Laravel, CI Framework).`,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: BuildingOffice2Icon},
+    {label: 'Location', text: 'Bali, Indonesia', Icon: MapIcon},
+    {label: 'Age', text: '28', Icon: CalendarIcon},
+    {label: 'Nationality', text: 'Indonesian', Icon: FlagIcon},
+    {label: 'Interests', text: 'Gaming, Cars, Bycyle', Icon: SparklesIcon},
+    {label: 'Study', text: 'Budi Luhur University', Icon: AcademicCapIcon},
+    {label: 'Employment', text: 'Hybrid Booking', Icon: BuildingOffice2Icon},
   ],
 };
 
@@ -126,15 +118,11 @@ export const skills: SkillGroup[] = [
     skills: [
       {
         name: 'English',
+        level: 8,
+      },
+      {
+        name: 'Bahasa',
         level: 10,
-      },
-      {
-        name: 'French',
-        level: 4,
-      },
-      {
-        name: 'Spanish',
-        level: 3,
       },
     ],
   },
@@ -153,6 +141,18 @@ export const skills: SkillGroup[] = [
         name: 'GraphQL',
         level: 6,
       },
+      {
+        name: 'Rest API',
+        level: 8,
+      },
+      {
+        name: 'Angular JS',
+        level: 7,
+      },
+      {
+        name: 'VueJS',
+        level: 7,
+      },
     ],
   },
   {
@@ -160,31 +160,14 @@ export const skills: SkillGroup[] = [
     skills: [
       {
         name: 'Node.js',
+        level: 7,
+      },
+      {
+        name: 'Laravel',
         level: 8,
       },
       {
-        name: 'Rust',
-        level: 5,
-      },
-      {
         name: 'Golang',
-        level: 4,
-      },
-    ],
-  },
-  {
-    name: 'Mobile development',
-    skills: [
-      {
-        name: 'React Native',
-        level: 9,
-      },
-      {
-        name: 'Flutter',
-        level: 4,
-      },
-      {
-        name: 'Swift',
         level: 3,
       },
     ],
@@ -268,39 +251,76 @@ export const portfolioItems: PortfolioItem[] = [
  */
 export const education: TimelineItem[] = [
   {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
-  },
-  {
-    date: 'March 2003',
-    location: 'School of Business',
-    title: 'What did you study 101',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: 'September 2014 - May 2018',
+    location: 'Budi Luhur University',
+    title: 'Bachelor of Information Technology',
+    content: <p>Thesis on "Electronic Tickets Using One Time Password (OTP) and REST Web Service on the BluCampus Application”.</p>,
   },
 ];
 
 export const experience: TimelineItem[] = [
   {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
+    date: 'June 2024 - Present',
+    location: 'PT Reservasi Indonesia Sejahtera',
+    title: 'Senior Web Developer',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        - Understanding Existing Product. <br/>
+        - Mostly the product already out of date for Development and upgrade it to new system. <br/>
+        - Develop new system which easy to scale, maintain and develop for further needs. <br/>
+        - Build a new Team for New System. <br/>
+        - Brainstorming with Product Owner for New Product.
       </p>
     ),
   },
   {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
+    date: 'May 2022 - June 2024',
+    location: 'PT Informasi Teknologi Indonesia',
+    title: 'AI Web Developer',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        - Implemented New Technology for Supporting AI Chatbot. <br/>
+        - Create Drawable canvas inside the dashboard with Javascript. <br/>
+        - Collaborate with Backend Engineer for Drawing a Chatbot Tree. <br/>
+        - Create dynamic Template for Chatbot Needs
+      </p>
+    ),
+  },
+  {
+    date: 'January 2022 - May 2022',
+    location: 'PT. Jari Karya Indonesia',
+    title: 'Senior Front End Web Developer',
+    content: (
+      <p>
+        - Create New Product and Collaborate with Product Owner. <br/>
+        - Analyze Product Requirement. <br/>
+        - Manage Milestone for Development Team. <br/>
+        - Scalling the system.
+      </p>
+    ),
+  },
+  {
+    date: 'June 2020 - January 2022',
+    location: 'PT. Emas Optimasi Abadi',
+    title: 'Senior Front End Web Developer',
+    content: (
+      <p>
+        - Collaborate with UI/UX and Research team to do further improvement <br/>
+        - Maintain product compatibility across devices. <br/>
+        - Build a beautiful real-time notification for website (Using FCM Firebase Cloud Messsaging) <br/>
+        - Develop a flexible, scalable, and well-structured front-end architecture, along with the APIs to support it.
+      </p>
+    ),
+  },
+  {
+    date: 'April 2018 -June 2020',
+    location: 'PT Ngampooz Pintar Sejahtera',
+    title: 'Front End Web Developer',
+    content: (
+      <p>
+        - Create product from scratch. <br/>
+        - Implemented a new technology such as New Version of PHP using Framework Laravel (Laravel version 5.5 at that time). <br/>
+        - Implemented preventive maintenance strategies, Web Security and Reducing Downtime at higher concurrent user visit (50K user at most).
       </p>
     ),
   },
@@ -336,27 +356,27 @@ export const testimonial: TestimonialSection = {
 
 export const contact: ContactSection = {
   headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  description: 'Feel free to contact me.',
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
+      text: 'rizaldirahman0@gmail.com',
+      href: 'mailto:rizaldirahman0@gmail.com',
     },
     {
       type: ContactType.Location,
-      text: 'Victoria BC, Canada',
+      text: 'Denpasar Bali, Indonesia',
       href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
     },
     {
       type: ContactType.Instagram,
-      text: '@tbakerx',
+      text: '@rizaldirhmn',
       href: 'https://www.instagram.com/tbakerx/',
     },
     {
       type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      text: 'rizaldirhmn',
+      href: 'https://github.com/rizaldirhmn',
     },
   ],
 };
@@ -365,9 +385,9 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
-  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
-  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/reactresume/'},
-  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
+  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/rizaldirhmn'},
+  // {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'www.linkedin.com/in/rizaldi-rahman-98713628b'},
+  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/rizaldirhmn/'},
+  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/rizaldirhmn'},
 ];
